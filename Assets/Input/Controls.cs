2c5419e160c5d5a8784351acda66f183a0dd4326
Ethan <eb15032@ms.wasatch.edu>
2021-04-23 12:26:33 -0600

    Camera Controller complete

--- Assets/Input/Controls.cs
+++ Assets/Input/Controls.cs
@@ -19,78 +19,39 @@ public class @Controls : IInputActionCollection, IDisposable
             ""id"": ""6ce63e2a-fe7f-4a28-8210-52ab98d1dc3f"",
             ""actions"": [
                 {
-                    ""name"": ""Move"",
-                    ""type"": ""PassThrough"",
-                    ""id"": ""72de69fd-c0ea-42ca-b2d4-95213e676b1d"",
-                    ""expectedControlType"": ""Vector2"",
-                    ""processors"": """",
-                    ""interactions"": """"
-                },
-                {
                     ""name"": ""Look"",
                     ""type"": ""PassThrough"",
                     ""id"": ""aec5ad23-f982-41fe-8562-9e1653cef961"",
                     ""expectedControlType"": ""Vector2"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Reset"",
+                    ""type"": ""Button"",
+                    ""id"": ""43eff502-ce15-4e61-9f56-d36a56ad5748"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Shoot"",
+                    ""type"": ""Button"",
+                    ""id"": ""7c6cf64c-e009-4ef3-96a4-cb9cf3859533"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Teleport"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""143b2a00-3118-463b-81ec-89d4fe377200"",
+                    ""expectedControlType"": ""Vector3"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
-                {
-                    ""name"": ""2D Vector"",
-                    ""id"": ""94933df8-3584-4708-bf63-7e70a0e5cf1e"",
-                    ""path"": ""2DVector"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Move"",
-                    ""isComposite"": true,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": ""up"",
-                    ""id"": ""d4e957d1-bf14-4eae-81e3-e94a406b132f"",
-                    ""path"": ""<Keyboard>/w"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""down"",
-                    ""id"": ""79f3a5d0-9046-46aa-8294-e268dc1e6ee3"",
-                    ""path"": ""<Keyboard>/s"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""left"",
-                    ""id"": ""c2bf954b-57fc-46d2-93cd-554830b3e138"",
-                    ""path"": ""<Keyboard>/a"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""right"",
-                    ""id"": ""02b6d8ed-ef5d-40e9-abfe-84b31dc0f3a7"",
-                    ""path"": ""<Keyboard>/d"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
                 {
                     ""name"": """",
                     ""id"": ""060f4681-5e60-4cc8-9a90-ffc6b01c82e9"",
@@ -101,6 +62,28 @@ public class @Controls : IInputActionCollection, IDisposable
                     ""action"": ""Look"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""ebfb98cd-c769-4ecd-b549-54e42d1fe2c4"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Reset"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""7cd8e61f-cc9b-4a33-9ea2-cd1599189a37"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Shoot"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -109,8 +92,10 @@ public class @Controls : IInputActionCollection, IDisposable
 }");
         // Locomotion
         m_Locomotion = asset.FindActionMap("Locomotion", throwIfNotFound: true);
-        m_Locomotion_Move = m_Locomotion.FindAction("Move", throwIfNotFound: true);
         m_Locomotion_Look = m_Locomotion.FindAction("Look", throwIfNotFound: true);
+        m_Locomotion_Reset = m_Locomotion.FindAction("Reset", throwIfNotFound: true);
+        m_Locomotion_Shoot = m_Locomotion.FindAction("Shoot", throwIfNotFound: true);
+        m_Locomotion_Teleport = m_Locomotion.FindAction("Teleport", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -160,14 +145,18 @@ public class @Controls : IInputActionCollection, IDisposable
     // Locomotion
     private readonly InputActionMap m_Locomotion;
     private ILocomotionActions m_LocomotionActionsCallbackInterface;
-    private readonly InputAction m_Locomotion_Move;
     private readonly InputAction m_Locomotion_Look;
+    private readonly InputAction m_Locomotion_Reset;
+    private readonly InputAction m_Locomotion_Shoot;
+    private readonly InputAction m_Locomotion_Teleport;
     public struct LocomotionActions
     {
         private @Controls m_Wrapper;
         public LocomotionActions(@Controls wrapper) { m_Wrapper = wrapper; }
-        public InputAction @Move => m_Wrapper.m_Locomotion_Move;
         public InputAction @Look => m_Wrapper.m_Locomotion_Look;
+        public InputAction @Reset => m_Wrapper.m_Locomotion_Reset;
+        public InputAction @Shoot => m_Wrapper.m_Locomotion_Shoot;
+        public InputAction @Teleport => m_Wrapper.m_Locomotion_Teleport;
         public InputActionMap Get() { return m_Wrapper.m_Locomotion; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -177,29 +166,43 @@ public class @Controls : IInputActionCollection, IDisposable
         {
             if (m_Wrapper.m_LocomotionActionsCallbackInterface != null)
             {
-                @Move.started -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnMove;
-                @Move.performed -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnMove;
-                @Move.canceled -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnMove;
                 @Look.started -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnLook;
                 @Look.performed -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnLook;
                 @Look.canceled -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnLook;
+                @Reset.started -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnReset;
+                @Reset.performed -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnReset;
+                @Reset.canceled -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnReset;
+                @Shoot.started -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnShoot;
+                @Shoot.performed -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnShoot;
+                @Shoot.canceled -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnShoot;
+                @Teleport.started -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnTeleport;
+                @Teleport.performed -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnTeleport;
+                @Teleport.canceled -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnTeleport;
             }
             m_Wrapper.m_LocomotionActionsCallbackInterface = instance;
             if (instance != null)
             {
-                @Move.started += instance.OnMove;
-                @Move.performed += instance.OnMove;
-                @Move.canceled += instance.OnMove;
                 @Look.started += instance.OnLook;
                 @Look.performed += instance.OnLook;
                 @Look.canceled += instance.OnLook;
+                @Reset.started += instance.OnReset;
+                @Reset.performed += instance.OnReset;
+                @Reset.canceled += instance.OnReset;
+                @Shoot.started += instance.OnShoot;
+                @Shoot.performed += instance.OnShoot;
+                @Shoot.canceled += instance.OnShoot;
+                @Teleport.started += instance.OnTeleport;
+                @Teleport.performed += instance.OnTeleport;
+                @Teleport.canceled += instance.OnTeleport;
             }
         }
     }
     public LocomotionActions @Locomotion => new LocomotionActions(this);
     public interface ILocomotionActions
     {
-        void OnMove(InputAction.CallbackContext context);
         void OnLook(InputAction.CallbackContext context);
+        void OnReset(InputAction.CallbackContext context);
+        void OnShoot(InputAction.CallbackContext context);
+        void OnTeleport(InputAction.CallbackContext context);
     }
 }
---
+++ Assets/Scripts/Player/InputHandler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public static InputHandler instance;
+
+    private Controls controls;
+
+    public Vector2 move;
+
+    public Vector2 look;
+
+    public Vector2 teleport;
+
+    public bool fireDown = false;
+    public bool aimDown = false;
+    public bool reset = false;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        controls = new Controls();
+
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    void Start()
+    {
+     
+        controls.Locomotion.Look.performed += controls => look = controls.ReadValue<Vector2>();
+        controls.Locomotion.Teleport.performed += controls => teleport = controls.ReadValue<Vector2>();
+
+        controls.Locomotion.Shoot.performed += controls => fireDown = true;
+        controls.Locomotion.Shoot.canceled += controls => fireDown = false;
+
+        controls.Locomotion.Reset.performed += controls => reset = true;
+        controls.Locomotion.Reset.canceled += controls => reset = false;
+
+
+        //revmove "//" when the control is added 
+
+        // controls.Locomotion.Aim.performed += controls => aimDown = true;
+        // controls.Locomotion.Aim.canceled += controls => aimDown = false;
+
+    }
+}
